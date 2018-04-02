b293f8090ab21b4aacc231784bff0dbbfa4fec13
Hisaki Akaza <TheRed@users.noreply.github.com>
2018-04-02 16:02:21 +0900

    feat: 開発環境を示すリボンを左上に表示

--- src/devMark.js
+++ src/devMark.js
@@ -1,1 +1,12 @@
-console.log('hello world!')
+import style from 'github-fork-ribbon-css/gh-fork-ribbon.css'
+style.use()
+
+const $body = document.getElementsByTagName('body')[0]
+$body.insertAdjacentHTML(
+  'afterbegin',
+  '<span class="github-fork-ribbon left-top" id="dev-ribbon" title="development"></span>'
+)
+const $devRibbon = document.getElementById('dev-ribbon')
+$devRibbon.onclick = () => {
+  $body.removeChild($devRibbon)
+}
